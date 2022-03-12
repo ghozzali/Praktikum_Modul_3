a2540d44d1e1913bfb56423bbf825f453f1ec462
ghozzali <ghozal.perdana02@gmail.com>
2022-03-12 17:12:10 +0700

    first commit

--- Program.cs
+++ Program.cs
@@ -27,6 +27,44 @@
             }
         }
     }
+    class PosisiKarakterGame
+    {
+        enum State { Berdiri, Jongkok, Berdiri2 };
+        public void tombol()
+        {
+            State state = State.Berdiri;
+
+            String[] screenName = { "Berdiri", "tombol arah bawah ditekan", "tombol arah atas ditekan" };
+            do
+            {
+                Console.WriteLine("" + screenName[(int)state]);
+                Console.Write("Tekan Tombol : ");
+                String command = Console.ReadLine();
+                switch (state)
+                {
+                    case State.Berdiri:
+                        if (command == "TombolS")
+                        {
+                            state = State.Jongkok;
+                        }
+                        break;
+                    case State.Jongkok:
+                        if (command == "TombolW")
+                        {
+                            state = State.Berdiri2;
+                        }
+                        break;
+                    case State.Berdiri2:
+                        if (command == "Stop")
+                        {
+                            state = State.Berdiri;
+                        }
+                        break;
+                }
+            } while (state != State.Berdiri);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
